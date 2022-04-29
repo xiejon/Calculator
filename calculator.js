1b249c205bb8fc6f52b992846a9803d8edffaa9e
xiejon <jxie77@gmail.com>
2022-04-30 00:22:07 +0100

    added working equals button

--- calculator.js
+++ calculator.js
@@ -28,6 +28,7 @@ function operate(operator, num1, num2) {
 
 
 let displayVal = 0;
+let obj = [];   
 
 const container = document.querySelector('.container');
 const display = container.querySelector('.result');
@@ -40,32 +41,42 @@ for (const number of numbers) {
 
 // clear display button
 const clear = document.querySelector('.clear'); 
-clear.addEventListener('click', e => display.textContent = ''); 
+clear.addEventListener('click', e => { 
+    display.textContent = '';
+    obj = [];
+}); 
 
-
-
-
-const operators = document.querySelectorAll('.operator');
-
-let obj = [];
-
-for (const operator of operators) {
-    operator.addEventListener('click', operatorListener);
+function addToArray(num, operation) {
+    obj.push({number: num, operator: operation});
 }
+
 function operatorListener(e) {
     let operation = e.target.id;            
-    let firstNum = display.textContent;      
-
-    addToArray(firstNum, operation);
-
+    let firstNum = display.textContent;
+    if (firstNum.length != 0) {        
+        addToArray(firstNum, operation);
+    }
     display.textContent = '';
-
     console.log(obj);
 };
 
-function addToArray (num, operation) {
-    obj.push({number: num, operator: operation});
+const operators = document.querySelectorAll('.operator');                   
+for (const operator of operators) {
+    operator.addEventListener('click', operatorListener);
 }
+
+function equalsListener() {
+    const equal = document.querySelector('.equals');
+    equal.addEventListener('click', (e) => {
+        obj.push({number: display.textContent});        // push latest number to array
+        let result = operate(obj[0].operator, parseInt(obj[0].number), parseInt(obj[1].number));    // initialize result with first 2 array entries
+        for (let i = 1; i < obj.length - 1; i++){
+            result = operate(obj[i].operator, result, parseInt(obj[i + 1].number));
+        }
+        display.textContent = result;
+    });
+}
+equalsListener();
 
 console.log(obj);
 // ideas: array to store values? once operator is clicked, store number
